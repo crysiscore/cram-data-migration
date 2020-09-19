f9534d6c00968c6044835b47c157cef963a05c19
Agnaldo <agnaldosamuel3@mail.com>
2020-09-19 16:30:39 +0200

    criacao da funcao para migrar os labs

--- tests.R
+++ tests.R
@@ -1,18 +1,43 @@
-for (pat_index in 1:nrow(created_patients) ) {
+
+
+lab <- readxl::read_xls(path = 'data/bloodexport.xls',col_names = TRUE)
+
+df.lab <- filter(lab, nid==pat.nid) 
+
+
+
+composeLab <- function(patient.uuid,df.lab ){
   
-  pat.nid <- created_patients$nid[pat_index]
-  uuid <- created_patients$openmrs_status[pat_index]
-  
-  df_visits <- filter(patient_visits, nid==pat.nid) %>% arrange(datvisit)
-  
-  for (vis_index in 1:nrow(df_visits) ) {
-    visit <- df_visits[vis_index,]
-    json.ficha.clinica <- composeFichaClinica(df.visits = visit,openmrs.pat.uuid =uuid )
+  index=1 
+  pat_lab <- df.lab
+  uuid <-patient.uuid
+   
+
+  if(nrow(pat_lab)!=0){
     
-    if(!validate(json.ficha.clinica)){
-      print(paste0("nid: ",nid," - problem in json format"))
+    # there are only 27 listed lab testts in the file
+    for ( i in 1:27){
+      
+      examen       <- lab[[paste0("examen",i)]][index]
+
+      if(!is.na(examen)) # no more lab requests {
+        encounter_datetime <-examen
+      
+        alat     <- lab[[paste0("alat",i)]][index] 
+        hbsag    <- lab[[paste0("hbsag",i)]][index]    #Hepatites b antigen s
+        creatui  <- lab[[paste0("creatui",i)]][index]  #Creatinine umol/L
+        lc       <- lab[[paste0("lc",i)]][index]       #Linfocitos
+        lccd4    <- lab[[paste0("lccd4",i)]][index]    #cd4 Numerico
+        lccd4tlc <- lab[[paste0("lccd4tlc",i)]][index] #cd4 Percentual
+        hivload     <- lab[[paste0("hivload",i)]][index]
+        hemoglb     <- lab[[paste0("hemoglb",i)]][index]
+
+  
+      }
+   
+      
     }
+   
     
-  }
-  
+    } 
 }
